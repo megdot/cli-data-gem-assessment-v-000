291dcfe15555452537d56bdfe0b5223106e15cb4
Megan Dotto <megan.dotto@gmail.com>
2018-06-11 01:02:26 +0000

    automatically backed up by learn

---
+++ top-100-books/lib/top/100/books.rb
@@ -0,0 +1,9 @@
+require "top/100/books/version"
+
+module Top
+  module 100
+    module Books
+      # Your code goes here...
+    end
+  end
+end
---
+++ top-100-books/lib/top/100/books/version.rb
@@ -0,0 +1,7 @@
+module Top
+  module 100
+    module Books
+      VERSION = "0.1.0"
+    end
+  end
+end
